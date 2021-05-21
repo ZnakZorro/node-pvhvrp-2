367ed554650dd7e37ecd96d6ca57e0c17d3c25d8
ZnakZorro <znakzorro@gmail.com>
2021-05-21 10:46:05 +0200

    3 files modified

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // Run `node index.js` in the terminal
 
 
-let u = 'https://www.yr.no/api/v0/locations/2-3083828/forecast';
-fetch(u)
-.then(r => {return r.json()})
-.then(o => {
-  console.log(o);
-})
-.catch(e => {console.log(e)})
+let url = 'https://www.yr.no/api/v0/locations/2-3083828/forecast';
+
+import request from 'request';
+
+//let url = "https://www.reddit.com/r/popular.json";
+
+let options = {json: true};
+
+
+
+request(url, options, (error, res, body) => {
+    if (error) {
+        return  console.log(error)
+    };
+
+    if (!error && res.statusCode == 200) {
+        // do something with JSON, using the 'body' variable
+    };
+});
