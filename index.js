63cfa16f9a64a1fc570c9206e921933364b130fa
ZnakZorro <znakzorro@gmail.com>
2021-05-21 10:42:31 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 // Run `node index.js` in the terminal
 
-//const fs = require('fs');
-//const cheerio = require('cheerio');
-//import got from 'got';
 
-let promiseYRNO = new Promise((resolve, reject) => {
-  
-	got('https://www.yr.no/api/v0/locations/2-3083828/forecast').then(async response => {
-	  let ret = response.body;
-		resolve(ret);
-	});
+let u = 'https://www.yr.no/api/v0/locations/2-3083828/forecast';
+fetch(u)
+.then(r => {return r.json()})
+.then(o => {
+  console.log(o);
 })
-
-
-
-
-promiseYRNO.then((value) => {
-	console.log(value);
-
-});
-
+.catch(e => {console.log(e)})
