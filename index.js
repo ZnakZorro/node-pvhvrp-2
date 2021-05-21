95d73bef5d89c6e286d59f418452d45341aa3cc0
ZnakZorro <znakzorro@gmail.com>
2021-05-21 11:02:25 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 
-const request = require('request');
+var request = require('request');
 
-let url = 'https://www.yr.no/api/v0/locations/2-3083828/forecast';
+var url = 'https://www.yr.no/api/v0/locations/2-3083828/forecast';
 
-let options = {json: true};
+var options = {json: true};
 
 request(url, options, (error, res, body) => {
     if (error) {return  console.log(error)};
 
     if (!error && res.statusCode == 200) {
         console.log(typeof body);
-		for (let o in body){
+		for (var o in body){
 			console.log(o);
 		}
-		for (let o in body.dayIntervals){
+		for (var o in body.dayIntervals){
 			console.log(o,body.dayIntervals[o]);
 		}
 		
